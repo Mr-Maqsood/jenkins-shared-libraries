ecae94021dc7d182080c684ffb1e40618d51c537
Mr-Maqsood <mk253751@gmail.com>
2025-10-21 12:08:18 +0500

    Update buildImages.groovy

--- vars/buildImages.groovy
+++ vars/buildImages.groovy
@@ -1,5 +1,12 @@
 def call(String backendPath, String frontendPath, String imageTag) {
     env.BACKEND_TAG_DH  = "${env.DOCKERHUB_USER}/three-tier-app-backend:${imageTag}"
     env.FRONTEND_TAG_DH = "${env.DOCKERHUB_USER}/three-tier-app-frontend:${imageTag}"
+
     sh """
+        echo "🚀 Building Backend Docker Image..."
         docker build -t ${BACKEND_TAG_DH} ${backendPath}
+
+        echo "🚀 Building Frontend Docker Image..."
+        docker build -t ${FRONTEND_TAG_DH} ${frontendPath}
+    """
+}
